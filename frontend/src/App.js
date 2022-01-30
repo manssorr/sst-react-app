e34725308d7aae30e796d77a085d11ea5cb36f3f
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-31 00:05:14 +0200

    Setting up our React app

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Routes from "./Routes";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Scratch
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
