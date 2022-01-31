44e654a040da3bb1ea0f9652e950f04b9d09a411
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-31 03:56:00 +0200

    Adding an init error

--- src/get.js
+++ src/get.js
@@ -1,30 +1,26 @@
 import handler from "./util/handler";
 import dynamoDb from "./util/dynamodb";
 
-function allocMem() {
-	let bigList = Array(4096000).fill(1);
-	return bigList.concat(allocMem());
-}
+// Some faulty code
+dynamoDb.notExist();
 
 export const main = handler(async (event) => {
-	const params = {
-		TableName: process.env.tableName,
-		// 'Key' defines the partition key and sort key of the item to be retrieved
-		// - 'userId': Identity Pool identity id of the authenticated user
-		// - 'noteId': path parameter
-		Key: {
-			userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-			noteId: event.pathParameters.id
-		}
-	};
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+      noteId: event.pathParameters.id
+    }
+  };
 
-	const result = await dynamoDb.get(params);
-	if (!result.Item) {
-		throw new Error("Item not found.");
-	}
+  const result = await dynamoDb.get(params);
+  if ( ! result.Item) {
+    throw new Error("Item not found.");
+  }
 
-	allocMem();
-
-	// Return the retrieved item
-	return result.Item;
+  // Return the retrieved item
+  return result.Item;
 });
