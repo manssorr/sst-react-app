42f3dc5ac414d7d77114d5dc13d06c0c57f31a9a
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-31 03:57:46 +0200

    Reverting faulty code

--- src/get.js
+++ src/get.js
@@ -1,26 +1,23 @@
 import handler from "./util/handler";
 import dynamoDb from "./util/dynamodb";
 
-// Some faulty code
-dynamoDb.notExist();
-
 export const main = handler(async (event) => {
-  const params = {
-    TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
-    Key: {
-      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      noteId: event.pathParameters.id
-    }
-  };
+	const params = {
+		TableName: process.env.tableName,
+		// 'Key' defines the partition key and sort key of the item to be retrieved
+		// - 'userId': Identity Pool identity id of the authenticated user
+		// - 'noteId': path parameter
+		Key: {
+			userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+			noteId: event.pathParameters.id
+		}
+	};
 
-  const result = await dynamoDb.get(params);
-  if ( ! result.Item) {
-    throw new Error("Item not found.");
-  }
+	const result = await dynamoDb.get(params);
+	if (!result.Item) {
+		throw new Error("Item not found.");
+	}
 
-  // Return the retrieved item
-  return result.Item;
+	// Return the retrieved item
+	return result.Item;
 });
